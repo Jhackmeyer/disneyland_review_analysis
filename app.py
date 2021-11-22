90abd689b6a93733eddceb75ace888848719243a
jhackmeyer <jhackmeyer@ucla.edu>
2021-11-22 02:25:05 -0800

    Adding images to results

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/predict',methods=['POST'])
 def predict():
     # Get the data from the POST request.
@@ -28,14 +29,16 @@ def predict():
 
         if(output == 0):
             results = "We suggest visiting Disneyland in California!"
+            img_result = "https://thumbs.dreamstime.com/b/disneyland-walt-disney-statue-princess-castle-anaheim-california-136919187.jpg"
         elif(output == 1):
             results = "We predict you would love Disneyland in Hong Kong!"
+            img_result = "https://fortheloveofthemouse.com/wp-content/uploads/2020/01/40-2.jpg"
         elif(output == 2):
-            results = "We highly recommend you visit Disneyland in Paris!"
-
+            results = "We highly recommend you visit Disneyland in Paris"
+            img_result = "https://st.depositphotos.com/1846219/3398/i/600/depositphotos_33981847-stock-photo-beautiful-scene-in-disneyland-paris.jpg"
 
         print(output)
-        return render_template("results.html", results=results)
+        return render_template("results.html", results=results, img_result=img_result)
 
 
 
